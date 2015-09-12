0c2aaf4915a983db128953c5617957784cd2c8e0
jonstieg <jonstieg@gmail.com>
2015-09-12 16:51:35 -0700

    fixed width font

--- app/views.py
+++ app/views.py
@@ -8,10 +8,11 @@ def search():
     form = Search()
     rebound = ReBound()
     if form.validate_on_submit():
-        flights = rebound.reboundSearch(form.departingAirport.data, form.arrivingAirport.data, request.form['depDay'], request.form['retDay'], request.form['depStops'], request.form['retStops'])
-        flash(flights)
+        # flights = rebound.reboundSearch(form.departingAirport.data, form.arrivingAirport.data, request.form['depDay'], request.form['retDay'], request.form['depStops'], request.form['retStops'])
+        # flash(flights)
+        flash('hello')
     return render_template('search.html', 
                            title='Search',
                            form=form)
-    
+
     # https://www.google.com/flights/#search;f=SAN;t=JFK;d=2015-09-28;r=2015-10-02;s=1
