6d9d0a2f7023e2a2ef810806079612073832d966
jonstieg <jonstieg@gmail.com>
2015-09-12 11:23:05 -0700

    scroll to top after click

--- app/views.py
+++ app/views.py
@@ -3,21 +3,13 @@ from app import app
 from .forms import Search
 from .rebound import ReBound
 
-@app.route('/results')
-
-def results():
-    return render_template("results.html",
-                           title='Results',)
-
 @app.route('/', methods=['GET', 'POST'])
-@app.route('/search', methods=['GET', 'POST'])
 def search():
     form = Search()
     rebound = ReBound()
     if form.validate_on_submit():
         flights = rebound.reboundSearch(form.departingAirport.data, form.arrivingAirport.data, request.form['depDay'], request.form['retDay'], request.form['depStops'], request.form['retStops'])
         flash(flights)
-        return redirect('/results')
     return render_template('search.html', 
                            title='Search',
                            form=form)
