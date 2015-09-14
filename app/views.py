8062da2b0deacd64ce62da2b8f18bc2c26b8450a
jonstieg <jonstieg@gmail.com>
2015-09-13 20:56:09 -0700

    d

--- app/views.py
+++ app/views.py
@@ -8,15 +8,12 @@ def search():
     form = Search()
     rebound = ReBound()
     if form.validate_on_submit():
-        # flights = rebound.reboundSearch(form.departingAirport.data, form.arrivingAirport.data, request.form['depDay'], request.form['retDay'], request.form['depStops'], request.form['retStops'], request.form['departingTimeEarly'], request.form['departingTimeLate'], request.form['returningTimeEarly'], request.form['returningTimeLate'])
-        # flash(flights)
-        flash('sdfdfsdfsd www.google.com')
-        flash('sdfdfsdfsd')
-
+        flights = rebound.reboundSearch(form.departingAirport.data, form.arrivingAirport.data, request.form['depDay'], request.form['retDay'], request.form['depStops'], request.form['retStops'], request.form['departingTimeEarly'], request.form['departingTimeLate'], request.form['returningTimeEarly'], request.form['returningTimeLate'])
+        for (x in flights)
+            flash(x)
 
     return render_template('search.html', 
                            title='Search',
                            form=form)
-
 
     # https://www.google.com/flights/#search;f=SAN;t=JFK;d=2015-09-28;r=2015-10-02;s=1
