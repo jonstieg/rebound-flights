5b20fe31c9fb0051649101ca0f33800bd30cfdf5
jonstieg <jonstieg@gmail.com>
2015-09-21 22:19:09 -0700

    20 searches

--- app/views.py
+++ app/views.py
@@ -11,7 +11,7 @@ def search():
         rebound = ReBound()
         if form.validate_on_submit():
             dateGo = datetime.today()
-            weeksToSearch = 2
+            weeksToSearch = 20
             flash ('Results:')
             for x in range(1,weeksToSearch+1):
                 flights = rebound.reboundSearch(dateGo, form.departingAirport.data, form.arrivingAirport.data, request.form['depDay'], request.form['retDay'], request.form['depStops'], request.form['retStops'], request.form['departingTimeEarly'],request.form['depTimeEarlyAMPM'], request.form['departingTimeLate'], request.form['depTimeLateAMPM'], request.form['returningTimeEarly'], request.form['retTimeEarlyAMPM'], request.form['returningTimeLate'], request.form['retTimeLateAMPM'])
