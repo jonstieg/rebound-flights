f866182d82e256f1555d632b5b266ae47979e5b8
jonstieg <jonstieg@gmail.com>
2015-09-21 22:12:52 -0700

    s

--- app/rebound.py
+++ app/rebound.py
@@ -16,7 +16,7 @@ class ReBound():
     returningTimeLate = "23:59" if returningTimeLate == '' else str(int(returningTimeLate) + int(returningTimeLateAMPM)) + ":00"
     weeksToSearch = 1
 
-    time.sleep(35)
+    # time.sleep(35)
     
     api_key = "AIzaSyBZj9cQKwEbMVQoSAgbfP1nhntS7peg-Jw"
     url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" + api_key
