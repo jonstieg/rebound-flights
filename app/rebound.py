2d6f7021ad24e540a3cda7fccf22ffd1e15c9c60
jonstieg <jonstieg@gmail.com>
2018-09-26 14:38:12 -0700

    replace actual API search with dummy data

--- app/rebound.py
+++ app/rebound.py
@@ -1,83 +1,98 @@
-import json
-import requests
-import os
-import operator
+# import json
+# import requests
+# import os
+# import operator
 import time
 from datetime import datetime, date, timedelta
+from random import randint
 
 class ReBound():
-  def reboundSearch(self, dateGo, departingAirport, arrivingAirport, departingWeekday, returningWeekday, maxStopsDeparting, maxStopsReturning, departingTimeEarly, departingTimeEarlyAMPM, departingTimeLate, departingTimeLateAMPM, returningTimeEarly, returningTimeEarlyAMPM, returningTimeLate, returningTimeLateAMPM):
-    departingWeekday = int(departingWeekday)
-    returningWeekday = int(returningWeekday)
+    def reboundSearch(self, dateGo, departingAirport, arrivingAirport, departingWeekday, returningWeekday, maxStopsDeparting, maxStopsReturning, departingTimeEarly, departingTimeEarlyAMPM, departingTimeLate, departingTimeLateAMPM, returningTimeEarly, returningTimeEarlyAMPM, returningTimeLate, returningTimeLateAMPM):
+        theDate = datetime.today()
+        results = ["Results:"]
+        for x in range(1, 20):
+            results.append("On {:%b, %d %Y} your flight will cost $".format(theDate) + str(randint(192,877)))
+            theDate += timedelta(7)
+        results.append("Unfortunately, Google deprecated their search API so all of the above prices are dummy data but this is how the site used to work.")
+        return results
 
-    departingTimeEarly = "00:00" if departingTimeEarly == '' else str(int(departingTimeEarly) + int(departingTimeEarlyAMPM)) + ":00"
-    returningTimeEarly = "00:00" if returningTimeEarly == '' else str(int(returningTimeEarly) + int(returningTimeEarlyAMPM)) + ":00"
-    departingTimeLate = "23:59" if departingTimeLate == '' else str(int(departingTimeLate) + int(departingTimeLateAMPM)) + ":00"
-    returningTimeLate = "23:59" if returningTimeLate == '' else str(int(returningTimeLate) + int(returningTimeLateAMPM)) + ":00"
-    weeksToSearch = 1
+    # def reboundSearch(self, dateGo, departingAirport, arrivingAirport, departingWeekday, returningWeekday, maxStopsDeparting, maxStopsReturning, departingTimeEarly, departingTimeEarlyAMPM, departingTimeLate, departingTimeLateAMPM, returningTimeEarly, returningTimeEarlyAMPM, returningTimeLate, returningTimeLateAMPM):
+    #     departingWeekday = int(departingWeekday)
+    #     returningWeekday = int(returningWeekday)
 
-    # time.sleep(35)
+    #     departingTimeEarly = "00:00" if departingTimeEarly == '' else str(
+    #         int(departingTimeEarly) + int(departingTimeEarlyAMPM)) + ":00"
+    #     returningTimeEarly = "00:00" if returningTimeEarly == '' else str(
+    #         int(returningTimeEarly) + int(returningTimeEarlyAMPM)) + ":00"
+    #     departingTimeLate = "23:59" if departingTimeLate == '' else str(
+    #         int(departingTimeLate) + int(departingTimeLateAMPM)) + ":00"
+    #     returningTimeLate = "23:59" if returningTimeLate == '' else str(
+    #         int(returningTimeLate) + int(returningTimeLateAMPM)) + ":00"
+    #     weeksToSearch = 1
 
-    api_key = os.environ.get('googAPI') 
-    url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" + api_key
-    headers = {'content-type': 'application/json'}
 
-    #dateGo = datetime.today()
-    while dateGo.weekday() != departingWeekday: 
-        dateGo += timedelta(days=1)
-    dateReturn = dateGo + timedelta(days=1)
-    while dateReturn.weekday() != returningWeekday: 
-        dateReturn += timedelta(days=1)
-    data = {};
+    #     api_key = os.environ.get('googAPI')
+    #     url = "https://www.googleapis.com/qpxExpress/v1/trips/search?key=" + api_key
+    #     headers = {'content-type': 'application/json'}
 
-    for x in range(1,weeksToSearch+1):
-      params = {
-        "request": {
-          "slice": [
-            {
-              "origin": departingAirport,
-              "destination": arrivingAirport,
-              "date": (dateGo + timedelta(7 * x)).strftime('%Y-%m-%d'),
-              "maxStops": maxStopsDeparting,
-              "permittedDepartureTime": {
-                "earliestTime": departingTimeEarly,
-                "latestTime": departingTimeLate
-              }
-            },
-            {
-              "origin": arrivingAirport,
-              "destination": departingAirport,
-              "date": (dateReturn + timedelta(7 * x)).strftime('%Y-%m-%d'),
-              "maxStops": maxStopsReturning,
-              "permittedDepartureTime": {
-                "earliestTime": returningTimeEarly,
-                "latestTime": returningTimeLate
-              }
-            }
-          ],
-          "passengers": {
-            "adultCount": 1,
-          },
-          "solutions": 1,
-        }
-      }
+    #     #dateGo = datetime.today()
+    #     while dateGo.weekday() != departingWeekday:
+    #         dateGo += timedelta(days=1)
+    #     dateReturn = dateGo + timedelta(days=1)
+    #     while dateReturn.weekday() != returningWeekday:
+    #         dateReturn += timedelta(days=1)
+    #     data = {}
 
-      response = requests.post(url, data=json.dumps(params), headers=headers)
-      data[x]=response.json()
+    #     for x in range(1, weeksToSearch+1):
+    #         params = {
+    #             "request": {
+    #                 "slice": [
+    #                     {
+    #                         "origin": departingAirport,
+    #                         "destination": arrivingAirport,
+    #                         "date": (dateGo + timedelta(7 * x)).strftime('%Y-%m-%d'),
+    #                         "maxStops": maxStopsDeparting,
+    #                         "permittedDepartureTime": {
+    #                             "earliestTime": departingTimeEarly,
+    #                             "latestTime": departingTimeLate
+    #                         }
+    #                     },
+    #                     {
+    #                         "origin": arrivingAirport,
+    #                         "destination": departingAirport,
+    #                         "date": (dateReturn + timedelta(7 * x)).strftime('%Y-%m-%d'),
+    #                         "maxStops": maxStopsReturning,
+    #                         "permittedDepartureTime": {
+    #                             "earliestTime": returningTimeEarly,
+    #                             "latestTime": returningTimeLate
+    #                         }
+    #                     }
+    #                 ],
+    #                 "passengers": {
+    #                     "adultCount": 1,
+    #                 },
+    #                 "solutions": 1,
+    #             }
+    #         }
 
-    # write out results to file
-    # print json.dumps(data, indent=1)
+    #         response = requests.post(
+    #             url, data=json.dumps(params), headers=headers)
+    #         data[x] = response.json()
 
-    flights = {}
-    for x in range(1,weeksToSearch+1):
-      try:
-        flights[data[x]["trips"]["tripOption"][0]["slice"][0]["segment"][0]["leg"][0]["departureTime"]] = data[x]["trips"]["tripOption"][0]["saleTotal"][3:]
-      except:
-        pass
-    for key,value in sorted(flights.items(), key=operator.itemgetter(1)):
-      try:
-        print key + ", " + value
-        file1.write(key + ", " + value)
-      except:
-        pass
-    return sorted(flights.items(), key=operator.itemgetter(1))
+    #     # write out results to file
+    #     # print json.dumps(data, indent=1)
+
+    #     flights = {}
+    #     for x in range(1, weeksToSearch+1):
+    #         try:
+    #             flights[data[x]["trips"]["tripOption"][0]["slice"][0]["segment"][0]["leg"]
+    #                     [0]["departureTime"]] = data[x]["trips"]["tripOption"][0]["saleTotal"][3:]
+    #         except:
+    #             pass
+    #     for key, value in sorted(flights.items(), key=operator.itemgetter(1)):
+    #         try:
+    #             print key + ", " + value
+    #             file1.write(key + ", " + value)
+    #         except:
+    #             pass
+    #     return sorted(flights.items(), key=operator.itemgetter(1))
